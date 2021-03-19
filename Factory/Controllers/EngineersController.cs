bf97b336ac9ef27999ae115d77ac6586c3cdf64a
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-19 13:44:13 -0500

    create private db connection; pass into constructor

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,11 +1,15 @@
+using Factory.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Factory.Controllers
 {
-    public class EngineersController : Controller
-    {
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
 
-
-
-    }
+      public EngineersController(FactoryContext db)
+      {
+        _db = db;
+      }
+  }
 }
