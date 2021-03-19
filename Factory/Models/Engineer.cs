8eddc3c9cae120ed6d75d331d841a64d29ca6c45
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-19 10:45:16 -0500

    add ICollection using statement and proprerty; pass into new constructor, set to hashset

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
+
 namespace Factory.Models
 {
   public class Engineer
   {
+    public Engineer()
+    {
+      JoinEntities = new HashSet<Repair>();
+    }
+
     public int EngineerId { get; set; }
     public string Name { get; set; }
     public DateTime HireDate { get; set; }
+    public virtual ICollection<Repair> JoinEntities { get; }
   }
 }
