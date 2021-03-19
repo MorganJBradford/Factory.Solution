e7a7a4ecf83dd235e3647b778586ac9078e62b8a
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-19 13:17:08 -0500

    add PostEdit route

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -7,54 +7,66 @@ using System.Linq;
 
 namespace Factory.Controllers
 {
-    public class MachinesController : Controller
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
     {
-      private readonly FactoryContext _db;
-
-      public MachinesController(FactoryContext db)
-      {
-        _db = db;
-      }
-
-      public ActionResult Index()
-      {
-        return View(_db.Machines.ToList());
-      }
-
-      public ActionResult Create()
-      {
-        ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
-        return View();
-      }
-
-      [HttpPost]
-      public ActionResult Create(Machine machine, int EngineerId)
-      {
-        machine.InstallationDate = DateTime.Now;
-        _db.Machines.Add(machine);
-        _db.SaveChanges();
-        if (EngineerId != 0)
-        {
-          _db.Repair.Add(new Repair() { EngineerId = EngineerId, MachineId = machine.MachineId });
-        }
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-      }
-
-      public ActionResult Details(int id)
-      {
-        Machine thisMachine = _db.Machines
-          .Include(machine => machine.JoinEntities)
-          .ThenInclude(join => join.Engineer)
-          .FirstOrDefault(machine => machine.MachineId == id);
-        return View(thisMachine);
-      }
-
-      public ActionResult Edit(int id)
-      {
-        Machine thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
-        ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
-        return View(thisMachine);
-      }
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+      return View(_db.Machines.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine, int EngineerId)
+    {
+      machine.InstallationDate = DateTime.Now;
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      if (EngineerId != 0)
+      {
+        _db.Repair.Add(new Repair() { EngineerId = EngineerId, MachineId = machine.MachineId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Machine thisMachine = _db.Machines
+        .Include(machine => machine.JoinEntities)
+        .ThenInclude(join => join.Engineer)
+        .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Machine thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+      return View(thisMachine);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Machine machine, int EngineerId)
+    {
+      if (EngineerId != 0)
+      {
+        _db.Repair.Add(new Repair { EngineerId = EngineerId, MachineId = machine.MachineId });
+      }
+      _db.Entry(machine).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
 }
