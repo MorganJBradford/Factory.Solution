d40c0fb3e42c8ccb52907c70103895b005eec5ff
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-19 11:46:22 -0500

    add database connection; pass into controller constructor

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -4,8 +4,11 @@ namespace Factory.Controllers
 {
     public class MachinesController : Controller
     {
+      private readonly FactoryContext _db;
 
-
-
+      public MachinesController(FactoryContext db)
+      {
+        _db = db;
+      }
     }
 }
